339aed7f2a221549ed99bdaa702dba0e660f0d8e
mariareis67-aura <freitas.reis.maria@escola.pr.gov.br>
2026-04-13 07:45:33 -0300

    u

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("loginform");
+const messege = document. getElementById("messege");
+
+form.addEventListener("submit"),function(event){
+    event.preventDefault();
+    
+}
